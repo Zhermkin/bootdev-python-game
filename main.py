05daba9ee0aba4e6d51d15b762c0550b53923be2
Zhermkin <sunilasokan@hotmail.com>
2026-02-26 20:24:43 -0400

    Make object move with WASD

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame  
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -11,12 +12,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     time = pygame.time.Clock()
     dt = 0
+    Ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        Ship.update(dt)
+        Ship.draw(screen)
         pygame.display.flip()
         dt = (time.tick(60))/1000
 
